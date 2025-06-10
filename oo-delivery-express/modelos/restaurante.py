48e3b29d99bf1fb6724acd41ae98058111f5448a
Raimundo Vieira <raimundoruan61@gmail.com>
2025-06-10 01:08:52 -0300

    ADD: @property

--- oo-delivery-express/modelos/restaurante.py
+++ oo-delivery-express/modelos/restaurante.py
@@ -3,16 +3,22 @@ class Restaurante:
     def __init__(self, nome, categoria):
         self.nome=nome
         self.categoria=categoria
-        self.ativo=False
+        self._ativo=False
         Restaurante.restaurantes.append(self)
     
     def __str__(self):
-        return f'|Nome: {self.nome.ljust(15)} | Categoria: {self.categoria.ljust(15)} | Status: {self.ativo}|'
+        return f'| Nome: {self.nome.ljust(15)} | Categoria: {self.categoria.ljust(15)} | Status: {self.ativo}|'
     
     def listar_restaurantes():
+        print(f'| {'Nome do Restaurante'.ljust(25)} | {'Categoria do Restaurante'.ljust(25)} | {'Status do Restaurante'.ljust(25)} |')
         for restaurante in Restaurante.restaurantes:
-            print(f'| Nome: {restaurante.nome.ljust(15)} | Categoria: {restaurante.categoria.ljust(15)} | Status: {restaurante.ativo} |')
+            print(f'| {restaurante.nome.ljust(25)} | {restaurante.categoria.ljust(25)} |{restaurante.ativo.ljust(25)} |')
+    @property
+    def ativo(self):
+        return '✅'if self._ativo else '❎'
+
+
+
 restaurante_praca=Restaurante('Praça','Italiana')
 restaurante_pizza=Restaurante('Pizza Express','Italiana')
 Restaurante.listar_restaurantes()
-print(restaurante_praca)
