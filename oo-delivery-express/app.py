2dc043244466c2344056a4d3dc7f08dff0b6ed54
Raimundo Vieira <raimundoruan61@gmail.com>
2025-06-25 15:37:10 -0300

    Adicionando um cardápio.
    Classes:
    Itens do Cardápio
    Bebida
    Prato

--- oo-delivery-express/app.py
+++ oo-delivery-express/app.py
@@ -7,16 +7,18 @@ Serve como ponto de entrada para demonstrar o funcionamento do módulo modelos.r
 """
 
 from modelos.restaurante import Restaurante
+from modelos.cardapio.bebida import Bebida
+from modelos.cardapio.prato import Prato
 restaurante_praca=Restaurante('Praça','Goumert')
 restaurante_mexicano=Restaurante('Mexican Food','Mexicana')
 restaurante_japones=Restaurante('Japa','Japonesa')
-restaurante_praca.receber_avaliacao('Rai',10)
-restaurante_praca.receber_avaliacao('Matheus',2)
-restaurante_praca.receber_avaliacao('Thigas',7.5)
-restaurante_praca.receber_avaliacao('Marcelo',10)
+
+bebida_suco=Bebida('Suco de Melancia',9.0,'Grande')
+prato_paozinho=Prato('Pãozinho',2.0,'O melhor da região')
 restaurante_mexicano.alternar_estado()
 def main():
-    Restaurante.listar_restaurantes()
+    print(bebida_suco)
+    print(prato_paozinho)
 
 
 
